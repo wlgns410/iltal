aa1bdff7978dfb8d53eabaacc6641247ffc72e9b
wlgns410 <wlgns410@gmail.com>
2021-07-22 10:51:56 +0900

    [create signup view]
    - user signup
    - UPDATE HOST class onotoone field
    - UPDATE USER class
    - commit user model
    - modify convention
    - update unit test
    - create user name
    - update test code
    - test code convention revise
    - update convention

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,97 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase, Client
+
+from users.models import User
+
+class SignupViewTest(TestCase):
+    def setUp(self):
+        User.objects.create(
+            id          = 1,
+            email       = "wlgns432@gmail.com",
+            password    = "1234asdf!",
+            name        = "dmwkdm"
+        )
+
+    def test_signupview_post_success(self):
+        client = Client()
+        user = {
+            'email'     : 'wldkwmd122@gmail.com',
+            'password'  : 'a123aaaa!',
+            'name'      : 'dmwkdm'
+        }
+        response = client.post('/users/signup', json.dumps(user), content_type='application/json')
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(),
+            {
+                "MESSAGE" : "SUCCESS"
+            }
+        )
+
+    def test_signupview_post_invalid_email(self):
+        client = Client()
+        user = {
+            'email'     : 'wwwwwwgmail.com',
+            'password'  : 'a123aaaa!',
+            'name'      : 'dmwkdm'
+        }
+        response = client.post('/users/signup', json.dumps(user), content_type='application/json')
+        
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),
+            {
+                "MESSAGE" : "INVALID EMAIL"
+            }
+        )
+
+    def test_signupview_post_invalid_password(self):
+        client = Client()
+        user = {
+            'email'     : 'wwwwww@gmail.com',
+            'password'  : 'a123aaaa',
+            'name'      : 'dmwkdm' 
+        }
+        response = client.post('/users/signup', json.dumps(user), content_type='application/json')
+        
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),
+            {
+                "MESSAGE" : "INVALID PASSWORD"
+            }
+        )
+
+    def test_signupview_post_duplicated_email(self):
+        client = Client()
+        user = {
+            'email'       : "wlgns432@gmail.com",
+            'password'    : "1234asdf!",
+            'name'        : "dmwkdm"
+        }
+        response = client.post('/users/signup', json.dumps(user), content_type='application/json')
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),
+            {
+                "MESSAGE" : "EXIST EMAIL"
+            }
+        )
+
+    def test_signupview_post_invalid_keys(self):
+        client = Client()
+        user = {
+            'ddemail'    : 'wlgns432@gmail.com',
+            'password'   : '1234asdf!',
+            'name'       : 'dmwkdm',
+        }
+        response = client.post('/users/signup', json.dumps(user), content_type='application/json')
+        print(response)
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),
+            {
+                "MESSAGE": "KEY_ERROR"
+            }
+        )
+
+    def tearDown(self):
+        User.objects.all().delete()
--- users/views.py
+++ users/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+import json, re, bcrypt
 
-# Create your views here.
+from django.http      import JsonResponse
+from django.views     import View
+
+from users.models   import User
+
+class SignupView(View):
+    def post(self, request):
+
+        data = json.loads(request.body)
+        EMAIL_REGES    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+        PASSWORD_REGES = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,16}$'
+
+        try:
+
+            if not re.search(EMAIL_REGES, data["email"]):
+                return JsonResponse ({"MESSAGE":"INVALID EMAIL"}, status = 400)
+
+            if not re.search(PASSWORD_REGES, data["password"]):
+                return JsonResponse ({"MESSAGE": "INVALID PASSWORD"}, status = 400)
+
+            if User.objects.filter(email=data["email"]).exists():
+                return JsonResponse ({"MESSAGE":"EXIST EMAIL"}, status = 400)
+
+            hashed_passwored = bcrypt.hashpw(data["password"].encode('utf-8'), bcrypt.gensalt()).decode()
+
+            User.objects.create(
+                email           = data["email"],
+                password        = hashed_passwored,
+                name            = data["name"],
+            )
+
+            return JsonResponse ({"MESSAGE":"SUCCESS"}, status = 201)
+        except KeyError:
+            return JsonResponse ({"MESSAGE":"KEY_ERROR"}, status = 400)
