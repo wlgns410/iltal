2bfc1574eb42148007177e7f86f2329770b3d593
wlgns410 <wlgns410@gmail.com>
2021-07-23 15:41:12 +0900

    [create signin]
    - create signin
    - signin modify
    - login view 수정
    - modify settings.py
    - remove requirement's module

--- users/tests.py
+++ users/tests.py
@@ -1,8 +1,11 @@
-import json
+import json, bcrypt, jwt
 
-from django.test import TestCase, Client
+from django.http    import response
+from django.test    import TestCase, Client
 
-from users.models import User
+
+from users.models   import User
+from my_settings    import SECRET_KEY, ALGORITHM
 
 class SignupViewTest(TestCase):
     def setUp(self):
@@ -95,3 +98,95 @@ class SignupViewTest(TestCase):
 
     def tearDown(self):
         User.objects.all().delete()
+
+class SigninViewTest(TestCase):
+    def setUp(self):
+        password    = 'wlaa1234!'
+
+        User.objects.create(
+            id          = 1,
+            password    = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8'),
+            email       = 'BrendanEich@gmail.com',
+            name        = 'gimgimgim'
+        )
+
+        access_token= jwt.encode({"user_id": 1}, SECRET_KEY, ALGORITHM)
+
+    def tearDown(self):
+         User.objects.all().delete()
+
+    # token 검증
+    def test_signinview_post_success(self):
+
+        client = Client()
+        user = {
+            'email'    : 'BrendanEich@gmail.com',
+            'password' : 'wlaa1234!'
+        }
+
+        response       = client.post('/users/signin', json.dumps(user), content_type="application/json")
+        access_token   = response.json()['access_token']
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), 
+            {
+                'message'       : 'success',
+                'access_token'  : access_token
+            }
+        )
+
+    # keyerror
+    def test_signinview_post_keyerror(self):
+
+        client = Client()
+
+        user = {
+            'password' : 'wlaa1234!'
+        }
+
+        response = client.post('/users/signin', json.dumps(user), content_type="application/json")
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), 
+            {
+                "message" : "KEY_ERROR"
+            }
+        )
+
+    # password error
+    def test_signinview_post_not_password(self):
+
+        client = Client()
+
+        user = {
+            'password'  : 'wlaa1232!',
+            'email'     : 'BrendanEich@gmail.com',
+        }
+
+        response = client.post('/users/signin', json.dumps(user), content_type="application/json")
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), 
+            {
+                "message" : "INVALID_USER"
+            }
+        )
+
+    # email error
+    def test_signinview_post_does_not_existed_email(self):
+
+        client = Client()
+
+        user = {
+            'password'  : 'wlaa1234!',
+            'email'     : 'Brendah@gmail.com',
+        }
+
+        response = client.post('/users/signin', json.dumps(user), content_type="application/json")
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.json(), 
+            {
+                "message" : "INVALID_USER"
+            }
+        )
--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
-import json, re, bcrypt
+import json, re, bcrypt, jwt
 
 from django.http      import JsonResponse
 from django.views     import View
 
 from users.models   import User
+from my_settings    import SECRET_KEY, ALGORITHM
 
 class SignupView(View):
     def post(self, request):
@@ -34,3 +35,24 @@ class SignupView(View):
             return JsonResponse ({"MESSAGE":"SUCCESS"}, status = 201)
         except KeyError:
             return JsonResponse ({"MESSAGE":"KEY_ERROR"}, status = 400)
+
+class SigninView(View):
+    def post(self, request):
+
+        try:
+            data = json.loads(request.body)
+
+            if not User.objects.filter(email=data['email']).exists():
+                return JsonResponse({"message" : "INVALID_USER"}, status=401)
+
+            user = User.objects.get(email=data["email"])
+
+            if not bcrypt.checkpw(data["password"].encode("utf-8"), user.password.encode("utf-8")):
+                return JsonResponse({"message": "INVALID_USER"}, status=401)
+
+            access_token = jwt.encode({"user_id": user.id}, SECRET_KEY, ALGORITHM)
+
+            return JsonResponse({"message":"success","access_token": access_token}, status=200)
+
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status=400)
